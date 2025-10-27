d6878d96dce4752ecf1854a35b667f09f55cb9ea
dangk <dangk@KHANHDANG>
2025-10-27 05:47:59 +0300

    update app.py

--- app.py
+++ app.py
@@ -189,3 +189,6 @@ def ask_question():
         "matched": False,
         "answer": answer
     })
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
